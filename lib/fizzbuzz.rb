e49f42faecb4cae5e14b0b897fcf5236f19080d7
Rory Grieve <rhrg500@gmail.com>
2016-11-01 12:57:47 +0000

    passes all tests and functionality

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
-  if number == 3
-    'fizz'
-  elsif number == 5
-    'buzz'
-  elsif number == 15
+  if number % 3 == 0 && number % 5 == 0
     'fizzbuzz'
+  elsif number % 5 == 0 
+    'buzz'
+  elsif number % 3 == 0
+    'fizz'
   else
       number
   end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,13 +1,13 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-     expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "fizz" when passed multiple 3' do
+     expect(fizzbuzz(12)).to eq 'fizz'
   end
-  it 'returns "buzz" when passed 5' do
-     expect(fizzbuzz(5)).to eq 'buzz'
+  it 'returns "buzz" when passed multiple 5' do
+     expect(fizzbuzz(10)).to eq 'buzz'
   end
-  it 'returns "fizzbuzz" when passed 15' do
+  it 'returns "fizzbuzz" when passed multiple of 3 and 5' do
       expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
   it 'returns "number" when passed any other number' do
